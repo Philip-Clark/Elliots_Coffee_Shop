e654d5e832c0fc3089c1116c98de1c4cb3e0198b
Philip Clark <ContactPhilipClark@protonmail.com>
2022-06-14 21:47:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,14 +11,6 @@
 </head>
 
 <body>
-
-$GithubRawURI= "https://raw.githubusercontent.com/UserName/GitName/FileName.php”; 
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, $GithubRawURI); 
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-$data = curl_exec($ch); 
-curl_close($ch); 
  
 //remove the "<?php" from the file before running it 
 $data = substr($data, 5); 
